115fa3068a1f77890c17a82c2f41600620897904
Spencer Kingman <spencer.kingman@niche.com>
2025-05-19 22:05:56 -0400

    Create the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,209 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <main>
+      <section className="fact__container">
+        <h1>Andrew Carnegie Facts:</h1>
+      </section>
+
+      <section className="nav__container" id="nav">
+        <nav>
+          <ul>
+            <li className="nav__item">
+              <a className="nav__link" href="#gave-away">Gave Away</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#immigrated">Immigrated</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#steel">Steel</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#medal">Medal</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#dinosaur">Dinosaur</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#second-richest">Second Richest</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#fortune">Fortune</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#carnegie-steel">Carnegie Steel</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#homestead-strike">Homestead Strike</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#short">Short</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#scottish">Scottish</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#philanthropist">Philanthropist</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#marriage">Marriage</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#braddock">Braddock</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#death">Death</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#daughter">Daughter</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#jobs">Jobs</a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#parents">Parents</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+
+      <section className="fact__container" id="gave-away">
+        <p className="fact__text">
+          Andrew Carnegie gave away 90% of his wealth, an amount which would be
+          equal to about 10.9 billions dollars in today's money.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </section>
+
+      <section className="fact__container" id="immigrated">
+        <p className="fact__text">
+          Andrew Carnegie immigrated to what is Pittsburgh, Pennsylvania, United
+          States with his parents in 1848 at the age of 12.
+        </p>
+      </section>
+
+      <section className="fact__container" id="steel">
+        <p className="fact__text">
+          Andrew Carnegie built Pittsburgh's Carnegie Steel Company, which he
+          sold to J. P. Morgan in 1901 for $303,450,000 (equal to
+          $11,469,200,000 today); it formed the basis of the U. S. Steel
+          Corporation.
+        </p>
+      </section>
+
+      <section className="fact__container" id="medal">
+        <p className="fact__text">
+          The Carnegie Medal for the best children's literature published in the
+          UK was established in his name.
+        </p>
+      </section>
+
+      <section className="fact__container" id="dinosaur">
+        <p className="fact__text">
+          The dinosaur <i>Diplodocus carnegiei</i> (Hatcher) was named for Carnegie
+          after he sponsored the expedition that discovered its remains in the
+          Morrison Formation (Jurassic) of Utah.
+        </p>
+      </section>
+
+      <section className="fact__container" id="second-richest">
+        <p className="fact__text">
+          At the height of his career, Carnegie was the second-richest person in
+          the world, behind only John D. Rockefeller of Standard Oil.
+        </p>
+      </section>
+
+      <section className="fact__container" id="fortune">
+        <p className="fact__text">
+          While working for the railroad, Carnegie invested in various ventures,
+          including iron and oil companies, and made his first fortune by the
+          time he was in his early 30s.
+        </p>
+      </section>
+
+      <section className="fact__container" id="carnegie-steel">
+        <p className="fact__text">
+          In 1892, his primary holdings were consolidated to form Carnegie Steel
+          Company.
+        </p>
+      </section>
+
+      <section className="fact__container" id="homestead-strike">
+        <p className="fact__text">
+          The steel magnate considered himself a champion of the working man;
+          however, his reputation was marred by the violent Homestead Strike in
+          1892 at his Homestead, Pennsylvania, steel mill.
+        </p>
+      </section>
+
+      <section className="fact__container" id="short">
+        <p className="fact__text">Andrew Carnegie was only 5”3”.</p>
+      </section>
+
+      <section className="fact__container" id="scottish">
+        <p className="fact__text">
+          Andrew Carnegie was on vacation in Scotland during the Homestead
+          strike, but put his support behind Frick, who called in some 300
+          Pinkerton armed guards to protect the plant.
+        </p>
+      </section>
+
+      <section className="fact__container" id="philanthropist">
+        <p className="fact__text">
+          Carnegie funded the establishment of more than 2,500 public libraries
+          around the globe, donated more than 7,600 organs to churches worldwide
+          and endowed organizations dedicated to research in science, education,
+          world peace and other causes.
+        </p>
+      </section>
+
+      <section className="fact__container" id="marriage">
+        <p className="fact__text">
+          Andrew Carnegie was 51 years old when he married Louise Whitfield
+          (1857-1946), who was two decades his junior and the daughter of a New
+          York City merchant.
+        </p>
+      </section>
+
+      <section className="fact__container" id="braddock">
+        <p className="fact__text">
+          Carnegie opened his first steel mill—the J. Edgar Thomson Steel
+          Works—in 1875 in Braddock.
+        </p>
+      </section>
+
+      <section className="fact__container" id="death">
+        <p className="fact__text">
+          Carnegie died at age 83 on August 11, 1919, at Shadowbrook, his estate
+          in Lenox, Massachusetts. He was buried at Sleepy Hollow Cemetery in
+          North Tarrytown, New York.
+        </p>
+      </section>
+
+      <section className="fact__container" id="daughter">
+        <p className="fact__text">
+          Andrew Carnegie and his wife, Louise Whitfield, had one daughter,
+          Margaret Carnegie in 1897.
+        </p>
+      </section>
+
+      <section className="fact__container" id="jobs">
+        <p className="fact__text">
+          Ambitious and hard-working, Carnegie had several other jobs including
+          a messenger in a telegraph office and secretary and telegraph operator
+          for the superintendent of the Pittsburgh division of the Pennsylvania
+          Railroad.
+        </p>
+      </section>
+
+      <section className="fact__container" id="parents">
+        <p className="fact__text">
+          Carnegie's parents were Will, a handloom weaver, and Margaret, who did
+          sewing work for local shoemakers.
+        </p>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
